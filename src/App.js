56a3ef747716a9b65ebd04e129103ea2f06802da
april <panapril77@gmail.com>
2021-04-18 22:32:39 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import LessonPage from './pages/LessonPage';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return  (
+    <BrowserRouter>
+    <NavBar/>
+      <Switch>
+        <Route component={HomePage} path='/' exact />
+        <Route component={LessonPage} path='/lesson' />
+      </Switch>
+    </BrowserRouter>
+  );;
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TitlebarGridList() {
+  return <div>home</div>;
+}
