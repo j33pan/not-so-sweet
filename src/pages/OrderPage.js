c9ccff37616961e692202a6fd0d5bfa98757d65d
april <panapril77@gmail.com>
2021-04-27 17:01:35 -0400

    clean up order

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,26 +1,12 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
-import { AccountContext } from '../Account';
 
 const OrderPage = () => {
-  const { getSession, logout } = React.useContext(AccountContext);
-  const [status, setStatus] = React.useState(false);
-  React.useEffect(() => {
-    getSession()
-      .then((session) => {
-        console.log(session);
-        setStatus(true);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+
 
   return (
     (
     <div>
-      {status ? 'logged in' : 'not logged in'}
-      <Button onClick={logout}>Log out</Button>
+order
     </div>
   )
   );
